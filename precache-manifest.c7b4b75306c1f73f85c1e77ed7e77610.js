609a7f906470017f3a3fc2ea572106e6f3502ad3
Rustam Khananov <antmine2017@gmail.com>
2020-06-23 15:49:34 +0300

    Updates

---
+++ precache-manifest.c7b4b75306c1f73f85c1e77ed7e77610.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c085793342e910dc0048b399898209cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fd23a7a3d1ab82ab797",
+    "url": "/static/css/main.403c733d.chunk.css"
+  },
+  {
+    "revision": "bb8573f46d86b232fc7e",
+    "url": "/static/js/2.58c5784a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.58c5784a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd23a7a3d1ab82ab797",
+    "url": "/static/js/main.bfba5061.chunk.js"
+  },
+  {
+    "revision": "8f68bee2f3651753c7e9",
+    "url": "/static/js/runtime-main.dd78a477.js"
+  },
+  {
+    "revision": "f58f02440c03bcd368d72210ace3e7f7",
+    "url": "/static/media/bee.f58f0244.png"
+  },
+  {
+    "revision": "6f544f8ec8d19b13cfdbee7b6a526c66",
+    "url": "/static/media/flower.6f544f8e.png"
+  },
+  {
+    "revision": "2cf2bc7b1ebd8739b0a6a1f7b0175a31",
+    "url": "/static/media/forest.2cf2bc7b.webp"
+  },
+  {
+    "revision": "f80cca51960c181f7b3265b7cc8643cd",
+    "url": "/static/media/honey.f80cca51.png"
+  }
+]);
